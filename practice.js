2c9e9eb40c061698063f52f76db1754ca02c29a7
Nia Williams <nswilliams0908@gmail.com>
2018-08-28 15:49:58 +0000

    deleted a useless part

--- practice.js
+++ practice.js
@@ -84,3 +84,11 @@ in my web page:
 // Using comments to prevent execution of code is suitable for code testing. Adding // in front of a code line changes the code lines from an executable line to a comment.
 
 // JavaScript variables are containers for storing data values.
+// In programming, just like in algebra, we use variables (like price1) to hold values. In programming, just like in algebra, we use variables in expressions (total = price1 + price2).
+// All JavaScript variables must be identified with unique names.These unique names are called identifiers. Identifiers can be short names (like x and y) or more descriptive names (age, sum, totalVolume).
+// The general rules for constructing names for variables (unique identifiers) are:
+// Names can contain letters, digits, underscores, and dollar signs.
+// Names must begin with a letter
+// Names can also begin with $ and _ (but we will not use it in this tutorial)
+// Names are case sensitive (y and Y are different variables)
+// Reserved words (like JavaScript keywords) cannot be used as names
